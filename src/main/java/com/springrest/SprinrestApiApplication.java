8615828100dde4e2b2764dd5e5d30fa85545eebd
josielmorais <moraisjosiel1@gmail>
2021-08-12 22:14:36 -0300

    Criando o projeto Spring Boot

---
+++ src/main/java/com/springrest/SprinrestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinrestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinrestApiApplication.class, args);
+	}
+
+}
