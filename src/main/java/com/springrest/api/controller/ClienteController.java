3861f325c6fbfd90318d3cffd0f74834415143d9
josielmorais <moraisjosiel1@gmail>
2021-08-13 12:14:42 -0300

    Usando Jakarta Persistence (JPA)

--- src/main/java/com/springrest/api/controller/ClienteController.java
+++ src/main/java/com/springrest/api/controller/ClienteController.java
@@ -1,8 +1,9 @@
 package com.springrest.api.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,25 +14,15 @@ import com.springrest.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-	
-		var c1 = new Cliente();
-		c1.setId(1L);
-		c1.setEmail("moraisjosiel1@gmail.com");
-		c1.setNome("Josiel");
-		c1.setTelefone("86 99999 9999");
-		
-		var c2 = new Cliente();
-		c2.setId(1L);
-		c2.setEmail("moraisjoao@gmail.com");
-		c2.setNome("João");
-		c2.setTelefone("86 99999 9999");
+		return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
 		
 	
-		return Arrays.asList(c1,c2);
-	}
+		}
 
 }
--- src/main/java/com/springrest/domain/model/Cliente.java
+++ src/main/java/com/springrest/domain/model/Cliente.java
@@ -1,14 +1,42 @@
 package com.springrest.domain.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 
-public class Cliente {
+@Entity
+public class Cliente implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+//	@NonNull
+	//@Size(max = 60)
 	private String nome;
+	
+//	@NotBlank
+//	@Email
+//	@Size(max = 255)
 	private String email;
-	private String telefone;
+	
+//	@NotBlank
+//	@Size(max = 20)
+//	@Columns(name = "fone")
+	private String fone;
+	
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -28,11 +56,35 @@ public class Cliente {
 		this.email = email;
 	}
 	public String getTelefone() {
-		return telefone;
+		return fone;
 	}
 	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+		this.fone = telefone;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 	
 
